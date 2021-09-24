e047e02e617cdc1cdbace3ea9f8f94b89059ab49
Christian <cgulick@student.fitchburgstate.edu>
2021-09-23 20:52:53 -0400

    Added Script folder and started player controller

---
+++ MobileGameProject1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject posLeft;
+    public GameObject posRight;
+    public GameObject posMid;
+
+    private int row;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get player input
+        if(Input.GetKeyDown(KeyCode.A)){
+            
+        }
+    }
+}
